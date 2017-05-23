f04d771005a76b678473f97b11c483361d112b7c
sochetanie <allagorik2901@gmail.com>
2017-05-23 17:25:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+    validates :title, presence: true
+    validates :title, exclusion: ["Won't Believe", "Secret", /Top [0-9]*/i, "Guess", "True Facts"]
+
+
+
 end
